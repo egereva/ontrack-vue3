12483b81903a13e86b85ed34e3ac3d80e5a869bb
Egereva Marina <marina.egereva@gmail.com>
2024-11-14 18:08:50 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <header class="sticky top-0 z-20 flex items-center justify-between border-b bg-white p-3">
+    <a href="">
+      <img src="./assets/img/logo.png" alt="Logo" class="h-9">
+    </a>
+    <a href="">
+      <div>Day complete!</div>
+    </a>
+  </header>
+
+  <main>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit ut ex at, alias voluptate delectus exercitationem rem adipisci veritatis numquam, neque, eligendi ab placeat aliquid quo repellat minima quod. Saepe.
+  </main>
+</template>
